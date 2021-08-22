2da4feed8b83d8355abc55f6748dd4c3367cfce1
Ben Gottlieb <ben@standalone.com>
2021-08-22 05:38:43 -0500

    Initial Commit

---
+++ SimpleLog/SimpleLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleLogApp.swift
+//  SimpleLog
+//
+//  Created by Ben Gottlieb on 8/22/21.
+//
+
+import SwiftUI
+
+@main
+struct SimpleLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
